ac56b42e9999c45d7d6df2fceb2d56de28191453
Jonathan Krase <jonathan.krase@gmail.com>
2026-05-05 18:11:32 +0200

    make Headline editable again

--- src/App.vue
+++ src/App.vue
@@ -121,7 +121,7 @@ onMounted(() => {
 
 <template>
   <div class="wheel-container">
-    <h1 class="title-heading" ref="titleElement" spellcheck="false" @blur="validate" @keydown.enter="validate">{{ title }}</h1>
+    <h1 class="title-heading" ref="titleElement" spellcheck="false" contenteditable="true" @blur="validate" @keydown.enter="validate">{{ title }}</h1>
 
     <svg width="400" height="400" class="wheel-svg">
       <circle cx="200" cy="200" r="150" class="outer-circle" />
